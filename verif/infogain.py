67ebe9a5a0a4903b2e8bbf982b7ad69508677c99
John Lawson <john.lawson@usu.edu>
2024-08-26 03:40:19 -0600

    Added placeholder for the class that will evaluate performance of the forecasts (possibility)

---
+++ verif/infogain.py
@@ -0,0 +1,4 @@
+"""Evaluation of forecast performance using the information gain metric (reduction in uncertainty)
+
+Could use whether it's above or below a percentile of the observation archive - 1 bit (choice)
+"""
