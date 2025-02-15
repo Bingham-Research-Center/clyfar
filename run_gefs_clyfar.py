7d42770f65e7a14f9615c809ce63cf45eccc178e
John Lawson <john.rob.lawson@googlemail.com>
2025-02-14 17:27:29 -0700

    Fix for division by zero.

--- run_gefs_clyfar.py
+++ run_gefs_clyfar.py
@@ -131,7 +131,7 @@ def weighted_average(elevation, mask):
     sum_neighbors = ndimage.convolve(masked_elevation, kernel, mode='constant', cval=0.0)
 
     # Compute the average of the surrounding cells
-    avg_neighbors = sum_neighbors / neighbor_counts
+    avg_neighbors = np.divide(sum_neighbors,neighbor_counts)
 
     # Compute the final weighted average
     weighted_avg = (2 * elevation + avg_neighbors) / 3
